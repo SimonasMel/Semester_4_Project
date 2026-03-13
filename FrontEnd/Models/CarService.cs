44b1de68290144436a3544381ee52253e9cb17f0
Karolis Bagdonavičius <karka050505@gmail.com>
2026-03-13 11:24:53 +0200

    Add files via upload

---
+++ FrontEnd/Models/CarService.cs
@@ -0,0 +1,17 @@
+﻿using Test.Models;
+
+public class CarService
+{
+    // This list holds the cars you "Liked"
+    public List<Car> MatchedCars { get; set; } = new List<Car>();
+    public event Action? OnChange;
+
+    public void AddMatch(Car car)
+    {
+        if (!MatchedCars.Any(c => c.Id == car.Id))
+        {
+            MatchedCars.Add(car);
+            OnChange?.Invoke();
+        }
+    }
+}
