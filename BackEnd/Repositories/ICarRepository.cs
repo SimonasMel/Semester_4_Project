4c594ea0863dd4513ef0671d83220fec62e1337c
Simonas Melionas <melionas.alt2@gmail.com>
2026-03-13 10:08:50 +0200

    Project structure

---
+++ BackEnd/Repositories/ICarRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarApp.Models;
+
+namespace CarApp.Repositories
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetAllAsync();
+        Task<Car?> GetByIdAsync(string id);
+        Task AddAsync(Car car);
+        Task UpdateAsync(Car car);
+        Task DeleteAsync(string id);
+        Task<bool> ExistsAsync(string id);
+    }
+}
